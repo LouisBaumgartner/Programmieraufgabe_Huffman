9ebc94896134e3cf48c2be1a08d019f1a70b3478
Louis Baumgartner <louis.baumgartner@students.fhnw.ch>
2018-12-05 13:25:58 +0100

    quellenalhphabet array

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+
+        String hello = "hello";
+
+        int [] quellenalphabetHäufigkeit = new int [128];
+        String [] kanalalphabetHäufigkeit = new String [128];
+
+        //Häufigkeit für jedes zeichen zählen
+        for (int i = 0; i < hello.length() ; i++) {
+            quellenalphabetHäufigkeit[(int)hello.charAt(i)]++;
+        }
+
+
+
     }
 }
+
